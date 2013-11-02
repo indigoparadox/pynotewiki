cacc6bd3cdd7cb8fbac5fb6b62f364e49c44fe27
indigoparadox <indigo.repo@interfinitdynamics.info>
2013-11-02 18:27:59 -0400

    Initial revision skeleton.

---
+++ pynotewiki/viewer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+'''
+This file is part of PyNoteWiki.
+
+PyNoteWiki is free software: you can redistribute it and/or modify it under 
+the terms of the GNU Lesser General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+PyNoteWiki is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more 
+details.
+
+You should have received a copy of the GNU Lesser General Public License along
+with PyNoteWiki.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+import gtk
+import webkit
+
