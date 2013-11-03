393cd5c5ee02c726271c5261a3bf1d60f16cc5be
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2013-11-02 22:08:02 -0400

    Using regexes might not be the best idea, but it works for now.

--- pynotewiki/__init__.py
+++ pynotewiki/__init__.py
@@ -17,5 +17,5 @@ You should have received a copy of the GNU Lesser General Public License along
 with PyNoteWiki.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-__all__ = ['viewer', 'parser']
+__all__ = ['viewer', 'parser', 'wikiconfig']
 
