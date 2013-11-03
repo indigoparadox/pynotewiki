4757617689af9c03db66f81f62b03cab02ef8736
indigoparadox <indigo.repo@interfinitdynamics.info>
2013-11-03 18:51:13 -0500

    Added hg parser skeleton and CLI parsing to setup.

--- parsers/hg.py
+++ parsers/hg.py
@@ -17,35 +17,3 @@ You should have received a copy of the GNU Lesser General Public License along
 with PyNoteWiki.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
-from distutils.core import setup
-
-# Parse CLI arguments.
-
-with_hg = True 
-filtered_args = [] 
-for arg in sys.argv: 
-   if arg == "--without-hg": 
-      with_hg = False
-   else: 
-      filtered_args.append( arg ) 
-sys.argv = filtered_args
-
-# Assemble the parsers and pixmaps.
-
-data_files = [
-   ('/usr/share/pixmaps',['pynotewiki.png']),
-   ('/usr/lib/pynotewiki/parsers',['parsers/nbk.py']),
-]
-
-if with_hg:
-   data_files.append( ('/usr/lib/pynotewiki/parsers',['parsers/hg.py']) )
-
-setup(
-   name='pynotewiki',
-   # TODO: Figure out a way to grab the repo revision or something.
-   version='0.1',
-   packages=['pynotewiki'],
-   scripts=['pnw.py'],
-   data_files=data_files,
-)
-
